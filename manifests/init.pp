2d83d5513a528489fb32e5eb9f46a581f67952ef
root <root@731400df031c.mylabserver.com>
2023-02-22 19:49:35 +0000

    creating module

---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+# @summary A short summary of the purpose of this class
+#
+#   include apache
+class apache {
+  include apache::install
+}
---
+++ manifests/install.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+# Installs base apache package
+
+
+class apache::install (
+  $install_name = $apache::params::install_name,
+  $install_ensure = $apache::params::install_ensure,
+
+) inherits apache::params {
+  package { "$install_name":
+     ensure => $install_ensure
+ }
+}
